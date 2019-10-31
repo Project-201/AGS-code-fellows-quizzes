f81c24ca276813fc2ba7905c536896c66edc20aa
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2019-10-31 10:24:36 +0200

    final touch

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ function showUserName() {
       welcomeDiv.setAttribute('style','display:block');
       inputDiv.setAttribute('style','display:none');
       exsamDiv.setAttribute('style','display:block');
-      newObject(userName);
+      newObject(userName,0,0,0);
       localStorage.setItem('userScore',JSON.stringify(allUsers));
     }else{
      
@@ -47,8 +47,8 @@ function showUserName() {
   }
 
 
-function newObject(name) {
-    var user = new Leader(name);
+function newObject(name, scoreForHtml, scoreForCss , scoreForJs , TotalAllScore) {
+    var user = new Leader(name, scoreForHtml, scoreForCss , scoreForJs , TotalAllScore);
     var users = localStorage.getItem('userScore');
     if(users){
         allUsers = JSON.parse(users); 
@@ -57,103 +57,3 @@ function newObject(name) {
        
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 'use strict';
-
-// // user constructor.
-// var Cart = function(user) {
-//   // this.items is an array of user.
-//   this.user = user;
-// };
-
-// // CartItem contructor.
-// var CartItem = function(product, quantity) {
-//   this.product = product;
-//   this.quantity = quantity;
-// };
-
-// // Product contructor.
-// var Product = function(filePath, name) {
-//   this.filePath = filePath;
-//   this.name = name;
-//   Product.allProducts.push(this);
-// };
-// Product.allProducts = [];
-
-// Cart.prototype.addItem = function(product, quantity) {
-//   var newCartItemInstance = new CartItem(product, quantity);
-//   this.user.push(newCartItemInstance);
-// };
-
-// Cart.prototype.saveToLocalStorage = function() {
-//   var itemString = JSON.stringify(this.items);
-//   localStorage.setItem('cart', itemString);
-// };
-
-// Cart.prototype.removeItem = function(item) {
-//   this.item.splice(item, 1);
-// };
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   addSelectedItemToCart();
-//   cart.saveToLocalStorage();
-//   showUserName();
-// }
-
-// function showUserName() {
-//   var selectedItemName = document.getElementById('items').value;
-//   var divOfSelectedItems = document.getElementById('cartContents');
-//   divOfSelectedItems.textContent = 'Welcome ' + selectedItemName + ', Wish you best of luck in your test.' + '<br>';
-//   divOfSelectedItems.setAttribute('style','display:block');
-// }
-
-// var userName = document.getElementById('userNameForm');
-// userForm.addEventListener('submit', handleOk);
-
-// function addSelectedItemToCart() {
-//   var selectedItemName = document.getElementById('items').value;
-//   var quantityValue = document.getElementById('quantity').value;
-//   // TODO: using those, add one item to the Cart
-//   // Done
-//   cart.addItem(selectedItemName, quantityValue);
-//   // var newItem = new Cart(selectedItemName, quantityValue);
-//   // cart.items.push(newItem);
-// }
-
-// function generateCatalog() {
-//   new Product('assets/bag.jpg', 'Bag');
-// }
-
-// // Initialize the app by creating the big list of products with images and names
-// generateCatalog();
